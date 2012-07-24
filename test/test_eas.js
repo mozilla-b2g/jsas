315f967288cbd7b4a687ba6d868134f652223834
Jim Porter <jporter@mozilla.com>
2012-07-23 20:10:24 -0500

    Start figuring out how to support syncing messages in ActiveSync

--- test/test_eas.js
+++ test/test_eas.js
@@ -13,11 +13,6 @@
  * limitations under the License.
  */
 
-function print(s) {
-  let output = document.getElementById("messages");
-  output.textContent += s;
-}
-
 function log(s) {
   let output = document.getElementById("log");
   output.textContent += s;
@@ -33,13 +28,10 @@ function logXhr(xhr) {
   log("\n");
 }
 
+var conn;
 window.addEventListener("load", function() {
-  print("Starting up...\n");
-
-  let conn = new ActiveSyncProtocol.Connection(
+  conn = new ActiveSyncProtocol.Connection(
     email, password, function(aResult) {
-      print(JSON.stringify(aResult, null, 2)+"\n\n");
-
       let fh = ActiveSyncCodepages.FolderHierarchy.Tags;
       let w = new WBXML.Writer("1.3", 1, "UTF-8");
       w.stag(fh.FolderSync)
@@ -48,18 +40,108 @@ window.addEventListener("load", function() {
 
       this.doCommand(w, function(aResponse) {
         let fh = ActiveSyncCodepages.FolderHierarchy.Tags;
+        let get_simple_node = function() {
+          let stag = aResponse.document.next();
+          if (stag.type != "STAG")
+            throw new Error("expected STAG node");
+
+          let text = aResponse.document.next();
+          if (text.type != "TEXT")
+            throw new Error("expected TEXT node");
+
+          if (aResponse.document.next().type != "ETAG")
+            throw new Error("expected ETAG node");
+
+          return [stag.localTagName, text.textContent];
+        };
+
+        let foldersNode = document.getElementById("folders");
         for (let node in aResponse.document) {
-          if (node.type == "STAG" && node.tag == fh.DisplayName) {
-            let text = aResponse.document.next();
-            if (text.type != "TEXT")
-              throw new Error("expected TEXT node");
-
-            print(text.textContent+"\n");
-
+          if (node.type == "STAG" && node.tag == fh.Add) {
+            let folderData = {};
+            for (let i = 0; i < 4; i++) {
+              let [key, value] = get_simple_node();
+              folderData[key] = value;
+            }
             if (aResponse.document.next().type != "ETAG")
               throw new Error("expected ETAG node");
+
+            let row = document.createElement("div");
+            row.className = "folder";
+            row.textContent = folderData.DisplayName;
+            row.addEventListener("click", function() {
+              getMessages(folderData);
+            }, false);
+            foldersNode.appendChild(row);
           }
         }
       });
     });
 }, false);
+
+function getMessages(folderData) {
+  let messagesNode = document.getElementById("messages");
+  messagesNode.innerHTML = "";
+
+  let folderName = document.createElement("h1");
+  folderName.textContent = folderData.DisplayName;
+  messagesNode.appendChild(folderName);
+
+  let as = ActiveSyncCodepages.AirSync.Tags;
+  let em = ActiveSyncCodepages.Email.Tags;
+
+  let w = new WBXML.Writer("1.3", 1, "UTF-8");
+  w.stag(as.Sync)
+     .stag(as.Collections)
+       .stag(as.Collection)
+         .tag(as.SyncKey, "0")
+         .tag(as.CollectionId, folderData.ServerId)
+       .etag()
+     .etag()
+   .etag();
+
+  conn.doCommand(w, function(aResponse) {
+    let syncKey;
+
+    for (let node in aResponse.document) {
+      if (node.type == "STAG" && node.tag == as.SyncKey) {
+          let text = aResponse.document.next();
+          if (text.type != "TEXT")
+            throw new Error("expected TEXT node");
+
+          if (aResponse.document.next().type != "ETAG")
+            throw new Error("expected ETAG node");
+
+        syncKey = text.textContent;
+      }
+    }
+
+    let w = new WBXML.Writer("1.3", 1, "UTF-8");
+    w.stag(as.Sync)
+       .stag(as.Collections)
+         .stag(as.Collection)
+           .tag(as.SyncKey, syncKey)
+           .tag(as.CollectionId, folderData.ServerId)
+           .tag(as.GetChanges)
+         .etag()
+       .etag()
+     .etag();
+
+    conn.doCommand(w, function(aResponse) {
+      for (let node in aResponse.document) {
+        if (node.type == "STAG" && node.tag == em.Subject) {
+          let text = aResponse.document.next();
+          if (text.type != "TEXT")
+            throw new Error("expected TEXT node");
+
+          if (aResponse.document.next().type != "ETAG")
+            throw new Error("expected ETAG node");
+
+          let message = document.createElement("div");
+          message.textContent = text.textContent;
+          messagesNode.appendChild(message);
+        }
+      }
+    });
+  });
+}
